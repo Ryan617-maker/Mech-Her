6124ba4207cde1c0d8607f52f76124ed73cd72a5
Ryan617-maker <ryandasilva233@gmail.com>
2025-11-12 17:28:20 -0300

    Create Card component with styling

---
+++ Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+export default function Card({ children }) {
+  return <View style={styles.card}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#fff',
+    borderRadius: 12,
+    padding: 16,
+    marginVertical: 8,
+    shadowColor: '#000',
+    shadowOpacity: 0.1,
+    shadowRadius: 5,
+    elevation: 3,
+  },
+});
